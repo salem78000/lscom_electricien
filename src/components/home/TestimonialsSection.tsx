3fbe799bbb9394840348ff3c346e451505e51cc7
salem78000 <salem78000@gmail.com>
2025-09-04 09:29:27 +0100

    Retirer le widget Elfsight de la section témoignages

--- src/components/home/TestimonialsSection.tsx
+++ src/components/home/TestimonialsSection.tsx
@@ -32,13 +32,6 @@ const TestimonialsSection: React.FC = () => {
             </p>
           </div>
           
-          {/* Elfsight Google Reviews Widget */}
-          <div 
-            className="elfsight-app-bc4c44a7-b903-40bc-aab5-1a857bd89cec" 
-            data-elfsight-app-lazy
-            style={{ minHeight: '400px' }}
-          ></div>
-          
           <div className="text-center mt-6">
             <a 
               href="https://www.google.com/search?q=LS+COM+électricien+avis" 
